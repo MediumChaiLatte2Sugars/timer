66728263a0e81917c9a9f018ff92e6f0c5d41ace
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-11 18:46:09 -0500

    Implemented timer with guard clauses

--- timer1.js
+++ timer1.js
@@ -1,9 +1,17 @@
 const arguments = process.argv.slice(2);
 
-// Assembling the alarms
-for (let alarm of arguments){
-  setTimeout(process.stdout.write('\x07'), alarm * 1000);
+// Check if we even have args
+if (arguments){
+
+  // Assembling the alarms
+  for (let alarm of arguments){
+    
+    // Skip negative numbers and non-numbers
+    if (alarm < 0 || !isNaN(alarm)){
+      continue;
+    }
+
+    setTimeout(() => { process.stdout.write('\x07'); }, alarm * 1000);
+  }
 }
 
-// // The sound we need!
-// process.stdout.write('\x07');
